cdfb973694a8adba52b1091059a328595ce5c65c
zhethon <kgb00128@gmail.com>
2019-05-18 22:31:00 +0800

    step 3： - bind 方法的應用，以及額外傳參 - JSX 內調用事件，添加列表，刪除列表 - 展開運算符 - immutable 概念

--- src/TodoList.js
+++ src/TodoList.js
@@ -21,11 +21,21 @@ class TodoList extends React.Component {
           {/* 將input的內容，和state數據庫進行雙向綁定 */}
           {/* 運用 ES6 語法中的 bind 函數，將 this 指向轉為整個組件 */}
           <input value={this.state.inputValue} onChange={this.handleInputChange.bind(this)}/>
-          <button>送出 Button</button>
+          <button onClick={this.handleBtnClick.bind(this)}>送出 Button</button>
         </div>
         <ul>
-            <li>Learning React</li>
-            <li>Learning English</li>
+          {
+            this.state.list.map((node, index) => {
+              return (
+                <li
+                  key={index}
+                  onClick={this.handleItemDelete.bind(this, index)}
+                >
+                  {node}
+                </li>
+              )
+            })
+          }
         </ul>
       </Fragment>
     )
@@ -34,6 +44,24 @@ class TodoList extends React.Component {
     // this.setState 是 react 規定用於改變數據的方法，必須調用此方法才能改變數據
     this.setState({
       inputValue: e.target.value
+    })
+  }
+  handleBtnClick () {
+    this.setState({
+      // ... 即是ES6的展開運算符，將陣列內的舊資料與新資料，重新組成新陣列
+      list: [...this.state.list, this.state.inputValue],
+      // 新增 todo-list 後，同時初始化 input
+      inputValue: ''
+    })
+  }
+  handleItemDelete (index) {
+    // 展開運算符，也可以理解為建立副本，在操作數據庫變化，建議透過副本操作，而非直接更動數據庫
+    // 在react 中有 immutable 這個概念，簡單說就是不要直接修改 state 內容，透過副本修改
+    let listCopy = [...this.state.list]
+    // 第一個參數，選擇要刪除的對應index，第二參數決定刪除此index後面幾個列表
+    listCopy.splice(index, 1)
+    this.setState({
+      list: listCopy
     })
   }
 }
