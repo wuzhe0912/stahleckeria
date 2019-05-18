7d5ebdbafd4145a2658a63b10b2003cf02f4528a
zhethon <kgb00128@gmail.com>
2019-05-18 14:57:51 +0800

    step 2： - 使用 state 儲存數據 - 使用 JSX 語法，需用{}包裹 - 注意 this 指向問題 - 在 react 中，事件名稱皆為駝峰格式 - 改變數據的方法，須遵行 react 要求的 setState

--- src/TodoList.js
+++ src/TodoList.js
@@ -2,22 +2,40 @@ import React, { Fragment } from 'react'
 // Fragment 是 React 提供一種佔位符，解決 JSX 語法返回必須在外層有一個 div 的問題
 // Fragment 在 chrome 開發者工具中的 Elements 不會被顯示
 
-function TodoList() {
-  return (
-    <Fragment>
-      <header>
-        Hello React!
-      </header>
-      <div className="wrap">
-        <input />
-        <button>Button</button>
+// 使用 class 的格式，是比較舊的寫法，在16.8之後版本，改直接調用函數
+class TodoList extends React.Component {
+  // 在類別，必然要建立建構式
+  constructor (props) {
+    // 建立一個 state 作為初始值與數據庫
+    super(props)
+    this.state = {
+      list: [],
+      inputValue: ''
+    }
+  }
+  // 準備組件要畫(渲染)出的內容
+  render () {
+    return (
+      <Fragment>
+        <div className="wrap">
+          {/* 將input的內容，和state數據庫進行雙向綁定 */}
+          {/* 運用 ES6 語法中的 bind 函數，將 this 指向轉為整個組件 */}
+          <input value={this.state.inputValue} onChange={this.handleInputChange.bind(this)}/>
+          <button>送出 Button</button>
+        </div>
         <ul>
-          <li>Learning English</li>
-          <li>Learning React</li>
+            <li>Learning React</li>
+            <li>Learning English</li>
         </ul>
-      </div>
-    </Fragment>
-  );
+      </Fragment>
+    )
+  }
+  handleInputChange (e) {
+    // this.setState 是 react 規定用於改變數據的方法，必須調用此方法才能改變數據
+    this.setState({
+      inputValue: e.target.value
+    })
+  }
 }
 
 export default TodoList;
