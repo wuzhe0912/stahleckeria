43395618f5517e4ab4c3dd9bc9a3e6a644551f12
zhethon <kgb00128@gmail.com>
2019-05-20 21:47:51 +0800

    step 7： - PropTypes 用於檢驗父組件傳值的類型 - 若加 isRequired 代表父組件必須強制傳值，若未傳值則跳 error - defaultProps 用於當作初始值，若父組件未傳值，則可以調用此處

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+// PropTypes 是用於檢驗傳值過來的屬性
+import PropTypes from 'prop-types'
 
 class TodoItem extends Component {
 
@@ -9,11 +11,11 @@ class TodoItem extends Component {
   }
 
   render () {
-    let { content } = this.props
+    let { content, test } = this.props
     return (
       // props 是子組件接收父組件參數的規定寫法
       <li onClick={this.handleClick}>
-        {content}
+        {content} - {test}
       </li>
     )
   }
@@ -21,6 +23,20 @@ class TodoItem extends Component {
     let { deleteItem, value } = this.props
     deleteItem(value)
   }
+}
+
+// 類似 Vue 的 prop 傳值，差別在於 Vue 需要指定傳值的類型，但 React 僅需檢驗
+// 若未傳值或傳空值，則不檢驗。但若添加 .isRequired
+TodoItem.propTypes = {
+  test: PropTypes.string.isRequired,
+  content: PropTypes.string,
+  deleteItem: PropTypes.func,
+  value: PropTypes.number
+}
+
+// 若父組件傳空 || 未傳值，則可調用此處的預設值
+TodoItem.defaultProps = {
+  test: 'Hello React'
 }
 
 export default TodoItem
